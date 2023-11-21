217446886441edae0a79a9a26398bc4e06e4ffb3
Zhilei Xu <astro.zlxu@gmail.com>
2023-11-20 21:59:30 -0500

    Adding name sound link.

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -14,13 +14,26 @@ const SideBar = () => (
       <header>
         <h2>Zhilei Xu</h2>
         <p><a href="mailto:astro.zlxu@gmail.com">astro.zlxu@gmail.com</a></p>
+        <p>Name Pronunciation
+          <audio controls>
+            <source src={`${PUBLIC_URL}/Zhilei_Xu_pron.mp4`} />
+            <track src={`${PUBLIC_URL}/Zhilei_Xu_pron.vtt`} kind="captions" srcLang="en" label="english_captions" />
+          </audio>
+        </p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hello, I am Zhilei Xu (sound Link to pronounce my name).
-      I am an astrophysicist interested in studying the beginning and the evolution of the Universe, especially through observing the       21-cm signal and cosmic microwave background (CMB). In addition, I am also interested in the formation and evolution of galaxies      and galaxy clusters. I am a member of the HERA collaboration in 21-cm and CLASS, SO, ACT, CMB-S4 collaborations in CMB.
+      <p>Hello, I am Zhilei Xu,
+        I am an astrophysicist interested in studying the beginning
+        and the evolution of the Universe,
+        especially through observing the 21-cm signal and
+        cosmic microwave background (CMB).
+        In addition, I am also interested in the formation and evolution
+        of galaxies and galaxy clusters.
+        I am a member of the HERA collaboration in 21-cm and CLASS, SO,
+        ACT, CMB-S4 collaborations in CMB.
       </p>
       <ul className="actions">
         <li>
