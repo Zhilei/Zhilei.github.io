98e1a1be948422c07a0c083d22bff1698c2bb2ab
Zhilei Xu <astro.zlxu@gmail.com>
2023-11-23 09:51:45 -0500

    Double to single quotation marks.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Zhilei Xu");
+    expect(document.title).toBe('Zhilei Xu');
   });
 
   it('can navigate to /about', async () => {
